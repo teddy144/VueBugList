7dafd64baf7aceca9bb4c0497807a9d085e0c96d
teddy144 <teppo.alstela@revelage.fi>
2020-10-21 00:35:56 +0300

    Add files via upload

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header class="header">
+            <h1>{{name}}</h1>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Header",
+        props: ["name"]
+    }
+</script>
+
+<style scoped>
+    .header{
+        background: #333;
+        color:white;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a{
+        color: white;
+        padding-right: 5px;
+    }
+</style>
